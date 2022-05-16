fb98da66d96cfb52a8ac2f379f96b322c3af99cb
Danny Logiurato <logiuratod@bzz.ch>
2022-05-16 19:21:29 +0200

    - Add Model Class - Add TestService

---
+++ src/main/java/ch/bzz/footballmanager/model/Club.java
@@ -0,0 +1,8 @@
+package ch.bzz.footballmanager.model;
+
+public class Club {
+    private int clubID;
+    private String name;
+    private String league;
+    private String stadium;
+}
---
+++ src/main/java/ch/bzz/footballmanager/model/Player.java
@@ -0,0 +1,12 @@
+package ch.bzz.footballmanager.model;
+
+public class Player {
+    private int playerID;
+    private String firstname;
+    private String lastname;
+    private String nationality;
+    private double height;
+    private double weight;
+    private double value;
+    private int phonenumber;
+}
