ae8f86447bac7f173075463ee12eb8f4f667993f
Danny Logiurato <logiuratod@bzz.ch>
2022-07-11 14:21:40 +0200

    add Testing

--- src/main/java/ch/bzz/footballmanager/model/Club.java
+++ src/main/java/ch/bzz/footballmanager/model/Club.java
@@ -1,16 +1,25 @@
 package ch.bzz.footballmanager.model;
 
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import javax.ws.rs.FormParam;
 
 public class Club {
     @FormParam("clubUUID")
+    @Pattern(regexp = "[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}")
     private String clubUUID;
+
     @FormParam("name")
+    @Size(min = 2, max = 45)
     private String name;
+
     @FormParam("league")
+    @Size(min = 2, max = 45)
     private String league;
+
     @FormParam("stadium")
+    @Size(min = 2, max = 45)
     private String stadium;
 
     /**
